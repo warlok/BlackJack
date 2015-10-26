ef7ac7ec27c752ba80106f14476221f89f4ec4b4
Denys <deanwhisper@gmail.com>
2015-10-26 22:07:08 +0200

    Initial

--- src/main/java/com/blackjack/PlayerDAO.java
+++ src/main/java/com/blackjack/PlayerDAO.java
@@ -1,7 +1,12 @@
 package com.blackjack;
 
+import com.blackjack.objects.Player;
+
 /**
  * Created by dean on 10/26/15.
  */
 public interface PlayerDAO {
+
+    public Player getUserByUserName(String userName);
+
 }
--- src/main/java/com/blackjack/objects/Player.java
+++ src/main/java/com/blackjack/objects/Player.java
@@ -4,4 +4,18 @@ package com.blackjack.objects;
  * Created by dean on 10/26/15.
  */
 public class Player {
+
+    private int userId;
+    private int groupId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int phoneNumber;
+    private String verificationCode;
+    private String resetPaswordCode;
+    private String passwordQuestion;
+    private String passwordAnswer;
+
 }
